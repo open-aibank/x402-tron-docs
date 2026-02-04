7dfacb41bfbca7698bbab4787bfa0758dddfec21
leo.wu <leo.wu@tron.network>
2026-02-04 17:28:24 +0800

    feat: support docusaurus

--- start_docker.sh
+++ start_docker.sh
@@ -2,4 +2,4 @@
 
 docker build --build-arg APP_ENV=prod -t x402-tron-docs .
 
-# docker run --name x402-tron-docs-container -p 8080:80 x402-tron-docs
+docker run --name x402-tron-docs-container -p 8081:80 x402-tron-docs
